7042ab51c864b03f53e8d98b771882cb11dee870
Dimitris Nioras <dimnioras@icloud.com>
2022-11-08 21:27:48 -0600

    Added index pages, logo, and configured locales

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,31 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default defineUserConfig({
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'el-GR',
+      title: 'Transport for Athens',
+      description: 'Ένα όραμα για μια καλύτερη συγκοινωνία στην Αθήνα και την Αττική',
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'Transport for Athens',
+      description: 'A vision for a better public transportation system in Athens',
+    },
+  },
+  head: [['link', { rel: 'icon', href: '/images/TransportForAthens_logo_favicon.png' }]],
+  theme: defaultTheme({
+    logo: '/images/TransportForAthens_logo_favicon.png',
+    locales: {
+      '/': {
+        selectLanguageName: 'Ελληνικά',
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+    },
+  }),
+})
---
+++ docs/en/index.md
@@ -0,0 +1,9 @@
+# Coming soon...
+
+## What is Transport for Athens?
+
+Transport for Athens is an effort to present a vision for a better public transportation system in Athens and the Attica region. In this website you will find recommendations about the upgrade and reorganization of the public transportation network in Athens based on transportation planning principles, the history and best practices, as well as the author's ideas. Moreover, you will find posts and critiques related to the current transportation system plan in Athens.
+
+## Author and Maintainer
+
+Dimitris Nioras, Transportation Planner and Engineer MS, ME
